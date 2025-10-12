70d8921549559bb0eb325043e1afd53cb13c493e
camilo noriega <hcnoriega@unicesar.edu.co>
2025-10-11 19:52:58 -0500

    agregue la capa gui en donde aparece la interfas de inicio

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GUI
+{
+    
+    public partial class MainWindow : Window 
+    {
+        public MainWindow()
+        {
+            
+        }
+
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+               
+                DragMove();
+            }
+        }
+
+        
+        private void IngresarButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("Has hecho clic en Ingresar a mi cuenta", "AgroSmart");
+        }
+
+        
+        private void CrearCuentaButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("Has hecho clic en Crear cuenta (registrarme)", "AgroSmart");
+        }
+    }
+}
